57b265ad8fe69c88e9f9cdb90c18c0dba7d71d77
Collin Taylor <cst21998@gmail.com>
2019-02-18 19:08:02 -0600

    :Sparkles: Add functions for greeter

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
-var nameChcker = function() {
-    var name = prompt("Enter your Name");
+var name;
+var blankChecker = function blankChecker(){
+    if(name === ''){
+        name = prompt('ENTER YOUR NAME NOW!');
+        blankChecker();
+    }
+};
+var nameChecker = function nameChecker(){
+    name = prompt('Enter your Name');
+    blankChecker();
+    document.querySelector('h1').textContent = `Hello, ${name}`;
+};
 
-    if (name === "") {
-    name = prompt("ENTER YOUR NAME NOW!");
-} else {
-document.querySelector("h1").textContent = `Hello, ${name}`;
-}
-}
-nameChcker();
+nameChecker();

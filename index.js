74856ab0bf9d70cfba5d9a33adc1e3cebd096d45
Collin Taylor <cst21998@gmail.com>
2019-02-26 20:24:57 -0600

    :hammer: Refactor to use bracket notation to avoid hard coded links

--- index.js
+++ index.js
@@ -3,59 +3,56 @@ import Header from './src/Header';
 import Content from './src/Content';
 import Footer from './src/Footer';
 
-var home = {
-    'title': "Welcome to my Savvy Coder's Website!"
+var State = {
+    'Home': {
+        'title': 'Welcome to Collin\'s Savvy Coder Website'
+    },
+    'Blog': {
+        'title': 'Read my words and stuff'
+    },
+    'Contact': {
+        'title': 'Contact Collin'
+    },
+    'Projects': {
+        'title': 'Behold my works, ye mighty, adn despair'
+    },
 };
-
-var blog = {
-    'title': 'Welcome to my blog'
-};
-
-var contact = {
-    'title': 'Welcome to my contact'
-};
-
-var projects = {
-    'title': 'Welcome to my projects!'
-};
-
 var root = document.querySelector('#root');
 
 function render(state){
+    var links;
+
     root.innerHTML = ` 
     ${Navigation(state)}
     ${Header(state.title)}
     ${Footer(state)}
     ${Content(state)}
 `;
+
+    links = document.querySelectorAll('#navigation > ul > li> a');
+
+    links[0].addEventListener('click', (event) => {
+        event.preventDefault();
+        render(State[event.target.textContent]);
+    });
+
+    links[1].addEventListener('click', (event) => {
+        event.preventDefault();
+
+        render(State[event.target.textContent]);
+    });
+
+    links[2].addEventListener('click', (event) => {
+        event.preventDefault();
+
+        render(State[event.target.textContent]);
+    });
+
+    links[3].addEventListener('click', (event) => {
+        event.preventDefault();
+
+        render(Stateevent.target.textContent);
+    });
 }
 
-render(home);
-
-document.querySelector('#navigation a').addEventListener(
-    'click', (e) => {
-        e.preventDefault();
-        console.log(e.target.href);
-        render(home);
-    });
-
-document.querySelector('#navigation li:nth-child(1) > a').addEventListener(
-    'click', (e) => {
-        e.preventDefault();
-        console.log(e.target.href);
-        render(blog);
-    });
-
-document.querySelector('#navigation li:nth-child(2) > a').addEventListener(
-    'click', (e) => {
-        e.preventDefault();
-        console.log(e.target.href);
-        render(contact);
-    });
-
-document.querySelector('#navigation li:nth-child(3) > a').addEventListener(
-    'click', (e) => {
-        e.preventDefault();
-        console.log(e.target.href);
-        render(projects);
-    });
+render(State.Home);
